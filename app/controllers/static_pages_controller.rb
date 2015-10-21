a3d185bc71e1cdf1b2282970e6ddccd2a07cab42
Thato Makinta <thatomkt@gmail.com>
2015-10-21 20:16:25 +0200

    added landing page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,3 +2,11 @@ class StaticPagesController < ApplicationController
   def index
   end
 end
+
+class StaticPagesController < ApplicationController
+
+  def landing_page
+  	@Featured_product = Product.first
+  end
+  
+end
