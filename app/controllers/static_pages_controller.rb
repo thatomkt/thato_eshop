754c8688c91ab808623b76244898854fbba1e2ae
Thato Makinta <thatomkt@gmail.com>
2015-10-29 21:44:40 +0200

    Edited Landing Page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,7 +5,7 @@ class StaticPagesController < ApplicationController
 
   def landing_page
   	@featured_product = Product.first
-  	@products = Product.limit(3)
+  	@products = Product.limit(10)
   end
   
 end
