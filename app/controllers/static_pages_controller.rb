1736b9c59076297a9312da4b0d0ae8fc9aa46cb9
Thato Makinta <thatomkt@gmail.com>
2015-11-09 19:40:11 +0200

    generated contact form sending emails

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,5 +7,15 @@ class StaticPagesController < ApplicationController
   	@featured_product = Product.first
   	@products = Product.limit(10)
   end
+
+  def thank_you
+  	@name = params[:name]
+  	@email = params[:email]
+  	@message = params[:message]
+  	ActionMailer::Base.mail(:from => @email,
+  		  :to => 'thatomkt@gmail.com',
+  		  :subject => "A new contact form message from #{@name}",
+  		  :body => @message).deliver_now
+  end
   
 end
