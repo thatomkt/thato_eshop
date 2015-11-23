8b923e5450e9ba3b669fc78a44be3f0c3246dccd
Thato Makinta <thatomkt@gmail.com>
2015-11-23 18:57:33 +0200

    comment form

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,7 @@
 class CommentsController < ApplicationController
 	def create
 	  @product = Product.find(params[:product_id])
-	  @comment = @product.comment.new(params[:comment])
+	  @comment = @product.comments.new(comment_params)
 	  @comment.user = current_user
 	  @comment.save
 	  redirect_to products_path(@product)
@@ -13,7 +13,7 @@ class CommentsController < ApplicationController
   private
 
     def comment_params
-    	params.require(:comment).permit(:user_id, :body :rating)
+    	params.require(:comment).permit(:user_id, :body, :rating)
     end
 
 
