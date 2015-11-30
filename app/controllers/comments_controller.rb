32e24953dd2010eac8d8c1d79288600d72f7a092
Thato Makinta <thatomkt@gmail.com>
2015-11-30 22:44:18 +0200

    Fixed the syntax error on comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,15 +4,16 @@ class CommentsController < ApplicationController
 	  @comment = @product.comments.new(comment_params)
 	  @comment.user = current_user
     respond_to do |format|
-	  if @comment.save
-	  redirect_to product_path(@product)
-       format.html {redirect_to @product, notice: 'Review was created successfully.'}
-       format.json {  render :show, status: :created, location: @product }
-     else
-        format.html { redirect_to @product, alert: 'Review was not saved successfully.'}
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
-  end
-end
+  	  if @comment.save
+  	     redirect_to product_path(@product)
+         format.html {redirect_to @product, notice: 'Review was created successfully.'}
+         format.json {  render :show, status: :created, location: @product }
+      else
+          format.html { redirect_to @product, alert: 'Review was not saved successfully.'}
+          format.json { render json: @comment.errors, status: :unprocessable_entity }
+      end
+    end
+  end  
 
 	def destroy
 	end
@@ -22,6 +23,5 @@ end
   def comment_params
   	params.require(:comment).permit(:user_id, :body, :rating)
   end
-
 
 end
