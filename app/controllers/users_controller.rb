2c00429ad70ee619548331966956ae936000c96e
Thato Makinta <thatomkt@gmail.com>
2015-12-03 22:48:28 +0200

    added admin flag to migration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -85,13 +85,7 @@ class UsersController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
       params.require(:user).permit(:first_name, :last_name)
-    end
-
-    class AddAdminFlagToUsers < ActiveRecord::Migration
-      def change
-        add_column :users, :admin, :boolean, default: false, null: false
-      end
-    end
+    end    
 
     class Ability
         include CanCan::Ability
