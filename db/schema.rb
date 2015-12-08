cd69be2b67a7d878e9f0c3bf093e72619aeb068d
Thato Makinta <thatomkt@gmail.com>
2015-12-08 20:04:09 +0200

    created spec files for user.rb and product.rb

--- db/schema.rb
+++ db/schema.rb
@@ -35,30 +35,30 @@ ActiveRecord::Schema.define(version: 20151202131526) do
   add_index "orders", ["user_id"], name: "index_orders_on_user_id"
 
   create_table "products", force: :cascade do |t|
-    t.string   "name",        limit: 255
+    t.string   "name"
     t.text     "description"
-    t.string   "image_url",   limit: 255
+    t.string   "image_url"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "colour",      limit: 255
+    t.string   "colour"
   end
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.boolean  "admin"
+    t.boolean  "admin",                  default: false, null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,7 +2,25 @@ require 'rails_helper'
 
 describe Product do
   
-  before do
-    @product = Product.create(:name => "champkidsbicycle")
-  end
+  context "when the product has comments"
+    before do
+      @product = Product.create!(:name => "champkidsbicycle")
+      @user =User.create!(:email => "thatomkt@me.com", :password => "Mntht@!1" )
+      @product.comments.create!(rating: 1, user: @user, body: "Awful bike!")
+      @product.comments.create!(rating: 3, user: @user, body: "Nice bike!")
+      @product.comments.create!(rating: 5, user: @user, body: "Very nice bike!")
+    end
+    it "returns the average rating of all comments" do
+      expect(@product.average_rating).to eq 3
+    end
+
+    context "when product has no name" do
+      before do
+       @product = Product.create(:description => "This is a test")
+      end
+
+      it 'is an invalid product' do
+        expect(@product).not_to be_valid
+      end
+    end
 end
